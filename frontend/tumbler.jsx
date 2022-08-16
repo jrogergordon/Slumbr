90074351633709f778ae1ce3816a59dfc0a450dc
Jacob <jrogergordon@gmail.com>
2022-08-16 12:14:57 -0400

    heroku

--- frontend/tumbler.jsx
+++ frontend/tumbler.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 document.addEventListener("DOMContentLoaded", () => {
+
     const root = document.getElementById("root");
     ReactDOM.render(<h1>Welcome to Tumbler</h1>, root);
 });
