0243a2e154977190a730be7c1c2473240838d5a0
Jacob <jrogergordon@gmail.com>
2022-08-16 10:09:23 -0400

    heroku issues

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
     context: __dirname,
-    entry: "./tumbler.jsx",
+    entry: "./frontend/tumbler.jsx",
     output: {
         path: path.resolve(__dirname),
         filename: "bundle.js"
