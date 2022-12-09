1b6bd87595189b359d18a683aa587dda099035c7
Jacob <jrogergordon@gmail.com>
2022-12-09 16:21:48 -0500

    render

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module Tumblr
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    config.railties_order = [:all, :main_app]
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
@@ -18,4 +20,4 @@ module Tumblr
   end
 end
 
-config.railties_order = [:all, :main_app]
+
