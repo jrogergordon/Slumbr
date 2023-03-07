e095ab4b16a027d25a79c231a87249ae04a2486c
Jacob <jrogergordon@gmail.com>
2023-03-06 23:46:12 -0500

    signin modal closes

--- frontend/reducers/modal_reducer.js
+++ frontend/reducers/modal_reducer.js
@@ -4,9 +4,7 @@ import { RECEIVE_CURRENT_USER } from '../actions/session_actions'
 
 
 export default function modalReducer(state = null, action) {
-    // console.log(action)
-    // console.log("modalReducer")
-    // console.log(RECEIVE_CURRENT_USER)
+
     switch (action.type) {
         case OPEN_MODAL:
             return action.modal;
